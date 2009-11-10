7489ff3aedcc05e7d2b294f6de97a9e7a496ffe1
redford <redford@ENCS-MAC-16.vancouver.wsu.edu>
2009-11-10 12:56:06 -0800

    Oops missed some packages, and if you run main class file, it'll show a crude game now.
    I added controls so you can control the player for now. Right now you can roam around.
    
    The box is supposed to be backghound. The large circle is SolidObject, which can be anything solid, like floor.

---
+++ src/gameObject/Player.java
@@ -0,0 +1,22 @@
+package gameObject;
+
+import jig.engine.physics.vpe.VanillaAARectangle;
+import jig.engine.util.Vector2D;
+
+public class Player extends VanillaAARectangle {
+	public Player(String a) {
+		super(a);
+		position = new Vector2D(0, 0);
+	}
+
+	public Player(String a, Vector2D init) {
+		super(a);
+		position = init;
+	}
+
+	@Override
+	public void update(long deltaMs) {
+		position = new Vector2D(position.getX() + (velocity.getX() * deltaMs)
+				/ 1000, position.getY() + (velocity.getY() * deltaMs) / 1000);
+	}
+}
