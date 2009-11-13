b3f659ecfae2b139875d49e34485a0a92a9039e1
rredford <Rolf@Redfords.net>
2009-11-13 13:50:13 -0800

    Player and Client changed to add basic fire command.

--- src/client/Player.java
+++ src/client/Player.java
@@ -1,15 +1,23 @@
 package client;
 
+import java.awt.Point;
 
 import net.NetObject;
 import jig.engine.physics.vpe.VanillaAARectangle;
 import jig.engine.util.Vector2D;
 
 public class Player extends VanillaAARectangle {
-	
+
 	private NetObject nObj;
 	private TcpClient tcp;
-	
+
+	private boolean facingLeft = true;
+
+	public Player() {
+		super("player");
+		position = new Vector2D(0, 0);
+	}
+
 	public Player(String a) {
 		super(a);
 		position = new Vector2D(0, 0);
@@ -21,29 +29,45 @@ public class Player extends VanillaAARectangle {
 		nObj = no;
 		tcp = c;
 	}
-	
+
 	public void moveLeft() {
 		Vector2D pos = nObj.getPosition();
-		tcp.sendMove(nObj.getId(), new Vector2D(pos.getX()-5, pos.getY()));
+		facingLeft = true;
+		tcp.sendMove(nObj.getId(), new Vector2D(pos.getX() - 5, pos.getY()));
 	}
-	
+
 	public void moveRight() {
 		Vector2D pos = nObj.getPosition();
-		tcp.sendMove(nObj.getId(), new Vector2D(pos.getX()+5, pos.getY()));
+		facingLeft = false;
+		tcp.sendMove(nObj.getId(), new Vector2D(pos.getX() + 5, pos.getY()));
 	}
-	
+
 	public void moveUp() {
 		Vector2D pos = nObj.getPosition();
-		tcp.sendMove(nObj.getId(), new Vector2D(pos.getX(), pos.getY()-5));
+		tcp.sendMove(nObj.getId(), new Vector2D(pos.getX(), pos.getY() - 5));
 	}
-	
+
 	public void moveDown() {
 		Vector2D pos = nObj.getPosition();
-		tcp.sendMove(nObj.getId(), new Vector2D(pos.getX(), pos.getY()+5));
+		tcp.sendMove(nObj.getId(), new Vector2D(pos.getX(), pos.getY() + 5));
 	}
-	
+
 	public void moveStop() {
-		setVelocity(new Vector2D(0,0));
+		setVelocity(new Vector2D(0, 0));
+	}
+
+	// Tests if facing the direction gun is pointing
+	// If so sends to server that weapon was fired.
+	// Otherwise nothing happens.
+	public void fireWeapon(Point point) {
+		double deltaX = position.getX() - point.x;
+		if (deltaX > 0 && facingLeft) {
+			System.out.println("Player fired weapon to left!");
+		} else if (deltaX < 0 && !facingLeft) {
+			System.out.println("Player fired weapon to right!");
+		} else {
+			System.out.println("Player tried to fire but was facing other way!");
+		}
 	}
 
 	@Override
